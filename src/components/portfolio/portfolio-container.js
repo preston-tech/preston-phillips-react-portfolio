4ea305e541b62c47ee2922aeb1334ae423b78c8f
preston-tech <prestonphillips42@gmail.com>
2019-12-18 14:32:21 -0700

    Added constructor to Portfolio

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,6 +3,11 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-items";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        console.log("Portfolio container has rendered");
+    }
     // State - class based component
     // Lifecycle hooks - managing data components
     render() {
