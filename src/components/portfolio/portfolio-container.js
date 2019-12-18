5aec1406d06d0749d3947c01dc539b2cce24419f
preston-tech <prestonphillips42@gmail.com>
2019-12-18 14:20:50 -0700

    class and functional components

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
+import PortfolioItem from "./portfolio-items";
+
 export default class PortfolioContainer extends Component {
+    // State - class based component
+    // Lifecycle hooks - managing data components
     render() {
         return (
             <div>
                 <h2>Portfolio items go here...</h2>
+
+                <PortfolioItem />
             </div>
         )
     }
