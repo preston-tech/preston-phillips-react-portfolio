3d7f62e8b7e45426252d58bf298d38248e45819e
preston-tech <prestonphillips42@gmail.com>
2019-12-18 15:00:50 -0700

    Custom function with map to loop over data

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,12 +10,21 @@ export default class PortfolioContainer extends Component {
     }
     // State - class based component
     // Lifecycle hooks - managing data components
+
+    portfolioItems() {
+        const data = ["Quip", "Eventbrite", "Ministry Safe", "Swing Away"];
+
+        return data.map(item => {
+            return <PortfolioItem />;
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio items go here...</h2>
 
-                <PortfolioItem />
+                {this.portfolioItems()}
             </div>
         )
     }
