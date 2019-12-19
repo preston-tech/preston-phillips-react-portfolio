1f86c8ef13634cab563018ea80611b61618ace75
preston-tech <prestonphillips42@gmail.com>
2019-12-19 15:19:08 -0700

    State changes

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -14,16 +14,24 @@ export default class PortfolioContainer extends Component {
             {title: "Quip"},
             {title: "Eventbrite"},
             {title: "Ministry Safe"},
-            {title: "Swing Away"},
+            {title: "Swing Away"}
             ]
         }
+
+        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
     }
-    
+
     // Lifecycle hooks - managing data components
 
     portfolioItems() {
         return this.state.data.map(item => {
             return <PortfolioItem title={item.title} url={"google.com"} />;
+        })
+    }
+
+    handlePageTitleUpdate() {
+        this.setState({
+            pageTitle: "Something Else"
         })
     }
 
@@ -33,6 +41,9 @@ export default class PortfolioContainer extends Component {
                 <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
+
+                <hr/>
+                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
             </div>
         )
     }
